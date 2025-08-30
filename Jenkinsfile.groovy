a5869cb38d47f670fafd5f7859f53a0b679550de
Thanh Phương <vipvippro608@gmail.com>
2025-08-30 15:41:22 +0700

    ok

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+    parameters {
+        string(name: 'TARGET_PATH', defaultValue: 'test.txt', description: 'File hoặc thư mục cần xoá')
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/vipvippro608-alt/phuongtest.git', branch: 'main', credentialsId: 'github-creds'
+            }
+        }
+
+        stage('Delete file') {
+            steps {
+                sh "rm -rf ${params.TARGET_PATH}"
+            }
+        }
+
+        stage('Commit & Push') {
+            steps {
+                sh """
+                git config user.name "Jenkins Bot"
+                git config user.email "jenkins-bot@example.com"
+                git add -A
+                git commit -m "auto delete ${params.TARGET_PATH}" || echo "Không có gì để commit"
+                git push origin main
+                """
+            }
+        }
+    }
+}
